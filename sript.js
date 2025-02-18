e7a4df60b0e52233c03770160007558624807421
abnerolimor <abner.oliveira.moraes@escola.pr.gov.br>
2025-02-18 17:00:13 -0300

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,13 @@
+var nome = prompt("Insira seu nome ");
+var email = prompt("Insira seu email");
+var cartao = prompt("Insira o número do verso do seu cartao de credito");
+var cartaoV = prompt("Insira a validade do seu cartao de credito");
+var cartaoS = prompt("Insira o numero de segurança do seu cartao de credito");
+var senha = prompt("Insira a senha");
+
+console.log(nome);
+console.log(email);
+console.log(cartao);
+console.log(cartaoS);
+console.log(cartaoV);
+console.log(senha);
